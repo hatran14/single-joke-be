a56ecfc07c87acc663d3846ec7ca9d157d2032d6
Ha Phan Hai <ha.phanhai4869@hcmut.edu.vn>
2024-04-02 00:02:30 +0700

    update: add middleware and routes to app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require("express")
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
 
 const { connectDB } = require("./config")
+const { v1 } = require("./routes")
 
 const app = express()
 
@@ -9,5 +14,14 @@ connectDB()
 app.get("/", (req, res) => {
 	res.send("Hello World!")
 })
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(cookieParser());
+
+v1(app)
 
 module.exports = app
