13b9420449b4f6531402612620e224cc47378be2
Ha Phan Hai <ha.phanhai4869@hcmut.edu.vn>
2024-04-02 09:57:00 +0700

    chore: add validation for joke text in createJoke method

--- src/controllers/joke.controller.js
+++ src/controllers/joke.controller.js
@@ -2,9 +2,12 @@ const { JokeService } = require("../services")
 
 class JokeController {
 	async createJoke(req, res) {
-    console.log('req.body', req.body)
+		const { text } = req.body || {}
+    if (!text) {
+      throw new Error("Joke text is required")
+    }
 		const joke = await JokeService.createJoke(req.body)
-		res.status(201).json(joke)
+		return res.status(201).json(joke)
 	}
 }
 
